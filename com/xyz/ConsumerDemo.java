0322874c218f0f4b79ba36aac9ba975611d0c6c1
kanishka-kumar <kanishkakumar23@gmail.com>
2025-05-17 16:33:11 +0530

    Using for-each along with consumer Chaining

--- com/xyz/ConsumerDemo.java
+++ com/xyz/ConsumerDemo.java
@@ -1,5 +1,9 @@
 package com.xyz;
 
+import com.xyz.data.Student;
+import com.xyz.data.StudentDataBase;
+
+import java.util.List;
 import java.util.function.Consumer;
 
 /*
@@ -8,8 +12,26 @@ Consumer Interface - introduced in Java 8. Part of java.util.function package
  */
 public class ConsumerDemo {
 
+    public static void printStudent(){
+        Consumer<Student> studentConsumer = student -> System.out.println(student);
+        List<Student> studentList = StudentDataBase.getAllStudents();
+        studentList.forEach(studentConsumer);
+    }
+
+    public static void printNameAndActivities(){
+        Consumer<Student> nameConsumer = student -> System.out.print(student.getName());
+        Consumer<Student> activitiesConsumer = student -> System.out.println(student.getActivities());
+
+        List<Student> studentList = StudentDataBase.getAllStudents();
+        studentList.forEach(nameConsumer.andThen(activitiesConsumer));//Consumer-Chaining
+    }
+
     public static void main(String[] args) {
         Consumer<String> consumer = (s) -> System.out.println(s.toUpperCase());
         consumer.accept("sample-text");
+        System.out.println("------------------\n------------------");
+        printStudent();
+        System.out.println("------------------\n------------------");
+        printNameAndActivities();
     }
 }
